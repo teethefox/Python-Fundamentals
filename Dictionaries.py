c2f892d757e8faa03552a3c372a9aff89d720aec
Tiffani Fox <teethefox@gmail.com>
2017-09-05 18:42:35 -0700

    Dictionary Basics

---
+++ Dictionaries.py
@@ -0,0 +1,14 @@
+"""Assignment: Making and Reading from Dictionaries
+Create a dictionary containing some information about yourself. The keys should include name, age, country of birth, favorite language.
+
+Write a function that will print something like the following as it executes:"""
+Dictionary Basics
+
+
+
+aDict = {}
+aDict["name"] = "Tiffanipack"
+aDict["age"]= "20"
+aDict["origin country"]= "USA"
+aDict["favlang"]= "ENGLISH/python"
+print "My name is", aDict["name"] +".\n My age is", aDict["age"]+".\n I hail from", aDict["origin country"]+".\n My favorite language is", aDict["favlang"]+"."
